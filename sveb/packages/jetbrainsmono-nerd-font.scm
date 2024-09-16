045a882711e98cddaeaaaf30ed82c73dacde8100
sveb <svebs@protonmail.com>
2024-09-16 20:43:54 +0200

    test

--- sveb/packages/jetbrainsmono-nerd-font.scm
+++ sveb/packages/jetbrainsmono-nerd-font.scm
@@ -27,11 +27,6 @@
   (package
     (name "JetBrainsMono-Nerd-Font")
     (version "3.2.1")
-    (source (origin
-	      (method url-fetch)
-	      (uri (string-append "https://github.com/ryanoasis/nerd-fonts/releases/download/v" version "/JetBrainsMono.zip"))
-	      (sha256
-		(base32 "6cf8822bc1ca18e34b06578c7499f380c019e6ffc883eed26df5f498dfcc4006"))))
     (build-system font-build-system)
     (synopsis "Patched JetBrainsMono Nerd Font")
     (home-page "https://github.com/ryanoasis/nerd-fonts")

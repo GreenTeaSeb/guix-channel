dbd292eb2ec8eda1e1fb8bc1c3ae77757ddfd93f
sveb <svebs@protonmail.com>
2024-09-16 22:41:05 +0200

    test

--- sveb/packages/fonts.scm
+++ sveb/packages/fonts.scm
@@ -12,7 +12,7 @@
     (source (origin
 	      (method url-fetch)
 	      (uri (string-append "https://github.com/ryanoasis/nerd-fonts/releases/download/v" version "/JetBrainsMono.zip"))
-	      (sha256 ("nt4iek6bzimogsygk6ghjgptqdabtzx7zcb65utn6x2jrx6miada")))
+	      (sha256 (base32 "nt4iek6bzimogsygk6ghjgptqdabtzx7zcb65utn6x2jrx6miada")))
     (build-system font-build-system)
     (synopsis "Patched JetBrainsMono Nerd Font")
 		(description "Font")

d3e3b14d108700ca86ebf65a81807e70a79b229f
sveb <svebs@protonmail.com>
2024-09-16 11:53:51 +0200

    font

---
+++ jetbrainsmono-nerd-font.scm
@@ -0,0 +1,19 @@
+(define-module (jetbrainsmono-nerd-font)
+	       #:use-module (guix packages)
+	       #:use-module (guix download)
+	       #:use-module (guix licenses))
+
+(define jetbrainsmono-nerd-font
+  (package
+    (name "JetBrainsMono Nerd Font")
+    (version "3.2.1")
+    (source (origin
+	      (method url-fetch)
+	      (uri (string-append "https://github.com/ryanoasis/nerd-fonts/releases/download/v" version "JetBrainsMono.tar.xz"))
+	      (sha256
+		(base32 "6cf8822bc1ca18e34b06578c7499f380c019e6ffc883eed26df5f498dfcc4006"))))
+    (build-system font-build-system)
+    (synopsis "Patched JetBrainsMono Nerd Font")
+    (home-page "https://github.com/ryanoasis/nerd-fonts")
+    (license license:silofl1.1)))
+    

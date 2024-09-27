21c42474e4db02164a4b27095e9e4e4bf0d4fbfd
skopsejak <sebastijan.kopsejak@b2match.com>
2024-09-28 00:21:37 +0200

    devbox info

--- sveb/packages/devbox.scm
+++ sveb/packages/devbox.scm
@@ -1,5 +1,5 @@
 (define-module (sveb packages devbox)
-              #:use-module ((guix licenses) #:prefix license:)
+              #:use-module (guix licenses)
  							#:use-module (guix packages)
  							#:use-module (guix download)
   						#:use-module (guix build-system go)
@@ -11,10 +11,17 @@
     (name "devbox")
     (version "0.13.1")
     (source (origin
-	      (method url-fetch)
-	      (uri (string-append "https://github.com/jetify-com/devbox/releases/download/v" version "/devbox_0.13.1_linux_amd64.tar.gz"))
-	      (sha256 (hex "1833736af5cc8d12080176da1255bb4ea3a97343dc81d117445ba5d2fa9d81b1"))))
+
+    (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/jetify-com/devbox")
+             (commit (go-version->git-ref version))))
+       (file-name (git-file-name name version))
+	      (sha256 (base64 "GDNzavXMjRIIAXbaElW7TqOpc0PcgdEXRFul0vqdgbE="))))
+
     (build-system go-build-system)
+    (arguments `(#:import-path "go.jetpack.io/devbox"))
+
     (synopsis "Development environment")
 		(description "Instant, easy, and predictable development environments ")
     (home-page "https://www.jetify.com/devbox")
